d78c6ec879aa8f6b4b985b3fe8b38aa941b0e3b4
Svyatoslav <slava-varov3@mail.ru>
2024-11-17 22:55:58 +0300

    refactor terrain shader

--- assets/shaders/terrain_fragment.glsl
+++ assets/shaders/terrain_fragment.glsl
@@ -1,43 +1,19 @@
-#version 330 core
+#version 450 core
 
-in float a_light;
-in vec2 a_TexCoord;
-in vec3 norm;
-in vec3 toCam;
-in vec3 v_toLightVec;
+in float v_light;
+in vec2 v_uv;
+in vec3 v_normal;
+in vec3 v_to_camera;
+in vec3 v_to_light;
 
-out vec4 color;
+out vec4 result_color;
 
-uniform sampler2D u_Texture0;
-uniform sampler2D u_Texture1;
+uniform sampler2D albedo_texture;
+uniform sampler2D normal_texture;
 uniform vec2 resolution;
-uniform vec3 lightColor;
+uniform vec3 light_color;
 
 void main() {
-    vec3 surfNorm = norm;
-
-    float shineDamper = 1000.0f;
-    float reflectivity = 0.25f;
-
-    vec4 texColor = texture(u_Texture0, a_TexCoord);
-
-    vec3 unitN = surfNorm;
-    vec3 unitL = normalize(v_toLightVec);
-    vec3 lightDir = -unitL;
-    vec3 unitC = normalize(toCam);
-
-    vec3 reflectedLight = reflect(lightDir, unitN);
-
-    float specularFactor = dot(reflectedLight, unitC);
-    specularFactor = max(specularFactor, 0.5f);
-    float dampedFactor = pow(specularFactor, shineDamper);
-    vec3 finalSpec = vec3(dampedFactor * lightColor * reflectivity);
-
-    float nDOTl = dot(unitN, unitL);
-    float brightness = max(nDOTl, 0.1f);
-    vec3 diffuse = vec3(brightness * lightColor);
-
-    vec3 mixcolor = mix(diffuse, texColor.rgb, brightness);
-
-    color = vec4(a_light * texColor.rgb, texColor.a);
+    vec4 albedo = texture(albedo_texture, v_uv);
+    result_color = vec4(v_light * albedo.rgb, albedo.a);
 }
