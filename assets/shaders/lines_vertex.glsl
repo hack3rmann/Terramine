48235fc2b39ab08b553c134927d668420a1ce248
Svyatoslav <slava-varov3@mail.ru>
2024-11-17 22:10:27 +0300

    refactor selection box shader

--- assets/shaders/lines_vertex.glsl
+++ assets/shaders/lines_vertex.glsl
@@ -1,14 +1,14 @@
-#version 330 core
-//#extension GL_ARB_separate_shader_objects : enable
+#version 450 core
 
-layout (location = 0) in vec3 a_position;
-layout (location = 1) in vec4 a_color;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 color;
 
-out vec4 v_Color;
+out vec4 v_color;
 
-uniform mat4 projView;
+uniform mat4 projection_view;
 
 void main() {
-	v_Color = a_color;
-	gl_Position = projView * vec4(a_position, 1.0f);
+    v_color = color;
+    gl_Position = projection_view * vec4(position, 1.0f);
 }
+
