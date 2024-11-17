8b6c5a4737a03ded59c7ec5dc9a329e4b86539a7
Svyatoslav <slava-varov3@mail.ru>
2024-11-17 22:29:30 +0300

    refactor postprocess shader

--- assets/shaders/postproc_fragment.glsl
+++ assets/shaders/postproc_fragment.glsl
@@ -1,82 +1,28 @@
-#version 330 core
+#version 450 core
 
-in vec2 a_TexCoord;
-out vec4 color;
+in vec2 v_uv;
+out vec4 result_color;
 
-uniform sampler2D screenColor;
-uniform int hue;
+uniform sampler2D depth_color_texture;
+uniform sampler2D screen_color_texture;
 uniform vec2 resolution;
+uniform float scaling;
 
-float m = 1.0f / 1080.0f;
+bool is_cross(float thickness, float size) {
+    bool is_vertical = abs(gl_FragCoord.x - 0.5 * resolution.x) <= 0.5 * thickness
+            && abs(gl_FragCoord.y - 0.5 * resolution.y) <= 0.5 * size;
 
-vec3 RGBtoHSL(vec3 inputColor) {
-    float Cmax = max(max(inputColor.r, inputColor.g), inputColor.b);
-    float Cmin = min(min(inputColor.r, inputColor.g), inputColor.b);
-    float delta = Cmax - Cmin;
+    bool is_horizontal = abs(gl_FragCoord.x - 0.5 * resolution.x) <= 0.5 * size
+            && abs(gl_FragCoord.y - 0.5 * resolution.y + 0.5) <= 0.5 * thickness;
 
-    float h, s, l;
-
-    l = (Cmin + Cmax) / 2.0f;
-
-    if (delta == 0.0f) {
-        h = 0.0f;
-        s = 0.0f;
-    } else {
-        s = delta / (1 - abs(2.0f * l - 1.0f));
-    }
-    if (Cmax == inputColor.r)
-        h = 60.0f * mod(((inputColor.g - inputColor.b) / delta), 6);
-    if (Cmax == inputColor.g)
-        h = 60.0f * (((inputColor.b - inputColor.r) / delta) + 2.0f);
-    if (Cmax == inputColor.b)
-        h = 60.0f * (((inputColor.r - inputColor.g) / delta) + 4.0f);
-
-    return vec3(h, s, l);
-}
-vec3 HSLtoRGB(vec3 inputColor) { // r->h, g->s, b->l
-    float c = inputColor.g * (1.0f - abs(2.0f * inputColor.b - 1.0f));
-    float x = c * (1 - abs(mod(inputColor.r / 60.0, 2.0f) - 1.0f));
-    float m = inputColor.b - c / 2.0f;
-    float h = inputColor.r;
-
-    vec3 res;
-
-    if (h >= 0.0f && h < 60.0f)
-        res = vec3(c, x, 0);
-    if (h >= 60.0f && h < 120.0f)
-        res = vec3(x, c, 0);
-    if (h >= 120.0f && h < 180.0f)
-        res = vec3(0, c, x);
-    if (h >= 180.0f && h < 240.0f)
-        res = vec3(0, x, c);
-    if (h >= 240.0f && h < 300.0f)
-        res = vec3(x, 0, c);
-    if (h >= 300.0f && h < 360.0f)
-        res = vec3(c, 0, c);
-
-    res = vec3(res.r + m, res.g + m, res.b + m);
-
-    return res;
+    return is_vertical || is_horizontal;
 }
 
 void main() {
-    vec4 inputColor = texture(screenColor, a_TexCoord);
-    vec3 hslCol = RGBtoHSL(inputColor.rgb);
-    //hslCol.g += hue / 360.0f;
-    //hslCol.r += hue;
+    vec4 screen_color = texture(screen_color_texture, v_uv);
+    result_color = screen_color;
 
-    color = vec4(HSLtoRGB(hslCol), 1.0);
-    color = inputColor;
-    // color = vec4(a_TexCoord, 0.0, 1.0);
-
-    if (gl_FragCoord.x >= resolution.x / 2 - 1 &&
-            gl_FragCoord.x <= resolution.x / 2 + 1 &&
-            gl_FragCoord.y >= resolution.y / 2 - 8 &&
-            gl_FragCoord.y <= resolution.y / 2 + 8 ||
-            gl_FragCoord.x >= resolution.x / 2 - 8 &&
-                gl_FragCoord.x <= resolution.x / 2 + 8 &&
-                gl_FragCoord.y >= resolution.y / 2 - 0.5 &&
-                gl_FragCoord.y <= resolution.y / 2 + 1) {
-        color = vec4(1.0 - color.rgb, 1.0);
+    if (is_cross(2.0 * scaling, 16.0 * scaling)) {
+        result_color = vec4(1.0 - result_color.rgb, 1.0);
     }
 }
--- assets/shaders/postproc_vertex.glsl
+++ assets/shaders/postproc_vertex.glsl
@@ -1,12 +1,11 @@
-#version 330 core
+#version 450 core
 
 layout(location = 0) in vec2 position;
 layout(location = 1) in vec2 uv;
 
-out vec2 a_TexCoord;
+out vec2 v_uv;
 
 void main() {
+    v_uv = uv;
     gl_Position = vec4(position, 0.0f, 1.0f);
-    a_TexCoord = uv;
 }
-
