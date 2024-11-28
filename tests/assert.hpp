ad53f556688f8c90790df3d8ac092158f26fde61
Svyatoslav <slava-varov3@mail.ru>
2024-11-28 22:00:12 +0300

    make panic macros better

--- tests/assert.hpp
+++ tests/assert.hpp
@@ -2,49 +2,42 @@
 
 #include <fmt/format.h>
 
-#define tmine_assert(expr)                                                    \
-    ({                                                                        \
-        if (!(expr)) {                                                        \
-            throw std::runtime_error(fmt::format(                             \
-                "test '{}' failed: assetion '{}' failed", __FUNCTION__, #expr \
-            ));                                                               \
-        }                                                                     \
+#define tmine_assert(expr, ...)                                              \
+    ({                                                                       \
+        if (!(expr)) {                                                       \
+            throw std::runtime_error(fmt::format(                            \
+                "test '{}' failed: assetion '{}' failed" __VA_OPT__(": {}"), \
+                __FUNCTION__, #expr __VA_OPT__(, fmt::format(__VA_ARGS__))   \
+            ));                                                              \
+        }                                                                    \
     })
 
-#define tmine_assert_msg(expr, msg...)                                      \
-    ({                                                                      \
-        if (!(expr)) {                                                      \
-            throw std::runtime_error(fmt::format(                           \
-                "test '{}' failed: assetion '{}' failed: {}", __FUNCTION__, \
-                #expr, fmt::format(msg)                                     \
-            ));                                                             \
-        }                                                                   \
+#define tmine_assert_eq(left, right, ...)                                      \
+    ({                                                                         \
+        auto const left_result = (left);                                       \
+        auto const right_result = (right);                                     \
+                                                                               \
+        if (left_result != right_result) {                                     \
+            throw std::runtime_error(fmt::format(                              \
+                "test '{}' failed: assertion '{} == {}' "                      \
+                "failed with left = '{}' and right = '{}'" __VA_OPT__(": {}"), \
+                __FUNCTION__, #left, #right, left_result,                      \
+                right_result __VA_OPT__(, fmt::format(__VA_ARGS__))            \
+            ));                                                                \
+        }                                                                      \
     })
 
-#define tmine_assert_eq(left, right)                                   \
-    ({                                                                 \
-        auto const left_result = (left);                               \
-        auto const right_result = (right);                             \
-                                                                       \
-        if (left_result != right_result) {                             \
-            throw std::runtime_error(fmt::format(                      \
-                "test '{}' failed: assertion '{} == {}' "              \
-                "failed with left = '{}' and right = '{}'",            \
-                __FUNCTION__, #left, #right, left_result, right_result \
-            ));                                                        \
-        }                                                              \
-    })
-
-#define tmine_assert_ne(left, right)                                   \
-    ({                                                                 \
-        auto const left_result = (left);                               \
-        auto const right_result = (right);                             \
-                                                                       \
-        if (left_result == right_result) {                             \
-            throw std::runtime_error(fmt::format(                      \
-                "test '{}' failed: assertion '{} != {}' "              \
-                "failed with left = '{}' and right = '{}'",            \
-                __FUNCTION__, #left, #right, left_result, right_result \
-            ));                                                        \
-        }                                                              \
+#define tmine_assert_ne(left, right, ...)                                      \
+    ({                                                                         \
+        auto const left_result = (left);                                       \
+        auto const right_result = (right);                                     \
+                                                                               \
+        if (left_result == right_result) {                                     \
+            throw std::runtime_error(fmt::format(                              \
+                "test '{}' failed: assertion '{} != {}' "                      \
+                "failed with left = '{}' and right = '{}'" __VA_OPT__(": {}"), \
+                __FUNCTION__, #left, #right, left_result,                      \
+                right_result __VA_OPT__(, fmt::format(__VA_ARGS__))            \
+            ));                                                                \
+        }                                                                      \
     })
