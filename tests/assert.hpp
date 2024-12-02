9169117b1936ab45d8d1bfe23c42c6c61f14f4a9
Svyatoslav <slava-varov3@mail.ru>
2024-12-02 15:26:37 +0300

    change `std::runtime_error(fmt::format(...))` to `Panic(...)`

--- tests/assert.hpp
+++ tests/assert.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <fmt/format.h>
+#include "panic.hpp"
 
 #define tmine_assert(expr, ...)                                              \
     ({                                                                       \
         if (!(expr)) {                                                       \
-            throw std::runtime_error(fmt::format(                            \
+            throw Panic(                                                     \
                 "test '{}' failed: assetion '{}' failed" __VA_OPT__(": {}"), \
                 __FUNCTION__, #expr __VA_OPT__(, fmt::format(__VA_ARGS__))   \
-            ));                                                              \
+            );                                                               \
         }                                                                    \
     })
 
@@ -18,12 +19,12 @@
         auto const right_result = (right);                                     \
                                                                                \
         if (left_result != right_result) {                                     \
-            throw std::runtime_error(fmt::format(                              \
+            throw Panic(                                                       \
                 "test '{}' failed: assertion '{} == {}' "                      \
                 "failed with left = '{}' and right = '{}'" __VA_OPT__(": {}"), \
                 __FUNCTION__, #left, #right, left_result,                      \
                 right_result __VA_OPT__(, fmt::format(__VA_ARGS__))            \
-            ));                                                                \
+            );                                                                 \
         }                                                                      \
     })
 
@@ -33,11 +34,11 @@
         auto const right_result = (right);                                     \
                                                                                \
         if (left_result == right_result) {                                     \
-            throw std::runtime_error(fmt::format(                              \
+            throw Panic(                                                       \
                 "test '{}' failed: assertion '{} != {}' "                      \
                 "failed with left = '{}' and right = '{}'" __VA_OPT__(": {}"), \
                 __FUNCTION__, #left, #right, left_result,                      \
                 right_result __VA_OPT__(, fmt::format(__VA_ARGS__))            \
-            ));                                                                \
+            );                                                                 \
         }                                                                      \
     })
